d8e094f9e68705817bb8d950917ca3f9c106f023
Tilchus <64080255+Tilchus@users.noreply.github.com>
2026-06-26 19:19:54 -0300

    Delete config_inicial.py

--- config_inicial.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-API_KEY = os.getenv("GOOGLE_API_KEY")
-ARCHIVO_TICKETS = r"C:\Users\Sil_Tech\Desktop\Proyecto_AgenteNexi_Modularizado\historial_tickets.json"
-MAX_INTENTOS = 3
